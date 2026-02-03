1338293ee501dba8793326afaf4870c5657001c6
charuK1002 <charukhandelwal610@gmail.com>
2026-02-03 09:12:03 +0530

    add pin configuration in ultrasonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
+#define trigpin 9
+#define echopin 10
 void setup() {
 // write your initialization code here
+    pinMode(trigpin,OUTPUT);
+    pinMode(echopin,INPUT);
 }
 
 void loop() {
